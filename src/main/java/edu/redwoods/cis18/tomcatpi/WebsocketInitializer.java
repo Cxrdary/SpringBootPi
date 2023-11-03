bda80b8d2937dc2b06a014a250338aa5d2b02d40
crazyne55 <95332356+crazyne55@users.noreply.github.com>
2023-11-02 17:06:15 -0700

    Starting to add websocket implementation. rest handler on root directory serves ui.html

---
+++ src/main/java/edu/redwoods/cis18/tomcatpi/WebsocketInitializer.java
@@ -0,0 +1,20 @@
+package edu.redwoods.cis18.tomcatpi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebsocketInitializer implements WebSocketConfigurer {
+
+    @Autowired
+    private MyWebSocketHandler myWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myWebSocketHandler, "/websocket/toUpper");
+    }
+}
